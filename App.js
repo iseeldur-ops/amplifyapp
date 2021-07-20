4b1b3fa5b79a6cae39813df37cfba855e8c2d953
iseeldur-ops <84935973+iseeldur-ops@users.noreply.github.com>
2021-07-20 16:28:10 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './zombie2.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
+        <p>
+          James Morgan - Personal AWS Test Website
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
